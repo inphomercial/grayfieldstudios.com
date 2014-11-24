c811801e94cb1626af6bfd13eac96e88d93ecebf
Sean Brown <inphomercial@gmail.com>
2014-11-24 15:33:56 -0700

    Added a php directory, got email submission working. got javascript loading and working.

--- js/greyfield.js
+++ js/greyfield.js
@@ -1,4 +1,44 @@
 
+var GREYFIELD = window.GREYFIELD || {};
 
-$(".splash").backstretch("img/background.jpg");
+// $(".splash").backstretch("img/background.jpg");
 
+/* ==================================================
+   Contact Form
+================================================== */
+GREYFIELD.contactForm = function(){
+	$("#contact-submit").on('click',function() {
+		$contact_form = $('#contact-form');
+
+        console.log($contact_form);
+
+		var fields = $contact_form.serialize();
+
+        console.log(fields);
+
+		$.ajax({
+			type: "POST",
+			url: "php/contact.php",
+			data: fields,
+			dataType: 'json',
+			success: function(response) {
+
+				if(response.status){
+                    $("#contact-form").find( "#name" ).val('');
+                    $("#contact-form").find( "#email" ).val('');
+                    $("#contact-form").find( "#message" ).val('');
+				}
+
+				$('#contact-error-name').empty().html(response.html.name);
+				$('#contact-error-email').empty().html(response.html.email);
+				$('#contact-error-message').empty().html(response.html.message);
+			}
+		});
+		return false;
+	});
+}
+
+$(document).ready(function() {
+    console.log("Loaded");
+    GREYFIELD.contactForm();
+});
